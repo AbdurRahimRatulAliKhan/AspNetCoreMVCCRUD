9e9173361b1e1dd01445802b21a8cce24406a438
Ratul Ali <rahim498617@gmail.com>
2023-10-20 23:10:54 +0600

    DbContext with model employee added

--- AspNetCoreMVCCRUD/Data/MVCDemoDbContext.cs
+++ AspNetCoreMVCCRUD/Data/MVCDemoDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace AspNetCoreMVCCRUD.Data
+﻿using AspNetCoreMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreMVCCRUD.Data
 {
-    public class MVCDemoDbContext
+    public class MVCDemoDbContext : DbContext
     {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
     }
+
 }
---
+++ AspNetCoreMVCCRUD/Models/Domain/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetCoreMVCCRUD.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Department { get; set; }
+        //public Employee() { }
+
+    }
+}
